9cc52610da0e5ff64d20ce67691025737726bc09
Picoleh <ltpicoli@gmail.com>
2026-06-16 15:42:01 +0200

    added fullscreen gallery

---
+++ Frontend/src/Components/Menus/Gallery/FullScreenGallery.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
+
+export default function FullScreenGallery({onClose, photos, openOn}: {onClose: () => void; photos: string[]; openOn: number}) {
+    const [currentIndex, setCurrentIndex] = useState(openOn);
+    return (
+        <div className="fixed inset-0 bg-black/90 z-50" onClick={onClose}>
+            <div className="h-full w-full flex flex-col items-center justify-between">
+                <div className="bg-gray-800/80 p-4 px-8 w-full flex flex-row justify-between items-center text-white" onClick={(e) => e.stopPropagation()}>
+                    <h2 className="text-2xl">Casa</h2>
+                    <div className="flex flex-row gap-12 items-center">
+                        <h2 className="text-2xl">{currentIndex + 1} / {photos.length}</h2>
+                        <div className="hover:bg-gray-600 text-3xl p-2 px-4 rounded-3xl" onClick={onClose}>
+                            <IoMdClose className="" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-row items-center justify-between w-full p-4 px-8">
+                    <div className="text-white bg-white/50 rounded-full p-2 flex justify-center items-center 
+                     hover:bg-white/70 hover:scale-105 cursor-pointer"
+                     onClick={(e) => {
+                         e.stopPropagation();
+                         setCurrentIndex((currentIndex - 1 + photos.length) % photos.length);
+                     }}>
+                        <FaChevronLeft className="text-4xl"/>
+                    </div>
+
+                    <img src={photos[currentIndex]} alt="Foto principal" className="max-w-full max-h-full object-contain" onClick={(e) => e.stopPropagation()} />
+
+                    <div className="text-white bg-white/50 rounded-full p-2 flex justify-center items-center 
+                     hover:bg-white/70 hover:scale-105 cursor-pointer"
+                     onClick={(e) => {
+                         e.stopPropagation();
+                         setCurrentIndex((currentIndex + 1) % photos.length);
+                     }}>
+                        <FaChevronRight className="text-4xl"/>
+                    </div>
+                </div>
+
+                <div className="bg-gray-800 p-4 w-full flex flex-row gap-8 items-center justify-center" onClick={(e) => e.stopPropagation()}>
+                    {photos.map((photo, index) => (
+                        <img 
+                            key={index}
+                            src={photo}
+                            alt={`Foto ${index + 1}`}
+                            className={`max-w-48 max-h-24 object-contain cursor-pointer border-2 hover:border-white transition 
+                                ${currentIndex === index ? 'border-orange-500' : ''}`}
+                            onClick={(e) => {
+                                setCurrentIndex(index);
+                            }}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
--- Frontend/src/Components/Menus/Gallery/GalleryPhotos.tsx
+++ Frontend/src/Components/Menus/Gallery/GalleryPhotos.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import FullScreenGallery from "./FullScreenGallery";
+
 const photos = [
   "https://www.jacksonsquareshopping.co.uk/wp-content/uploads/2016/12/placeholder-1920x1080-copy-3.png",
   "https://images.unsplash.com/photo-1494526585095-c41746248156?auto=format&fit=crop&w=800&q=80",
@@ -7,35 +10,57 @@ const photos = [
 ];
 
 export default function GalleryPhotos() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [openOn, setOpenOn] = useState(0);
+    const photoLimit = 5;
     return (
         <div className="grid grid-cols-1 md:grid-cols-4 grid-rows-2 gap-4 h-full rounded-3xl overflow-hidden">
+            {isOpen && <FullScreenGallery onClose={() => setIsOpen(false)} photos={photos} openOn={openOn}/>}
             {/* Principal */}
-            <div className="md:col-span-2 md:row-span-2">
+            <div className="md:col-span-2 md:row-span-2" onClick={() => {
+                setIsOpen(true);
+                setOpenOn(0);
+            }}>
                 <img src={photos[0]} alt="Foto principal" className="w-full h-full object-cover"/>
             </div>
 
             {/* Direita topo */}
             <div>
-                <img src={photos[1]} alt="Foto 2" className="w-full h-full object-cover"/>
+                <img src={photos[1]} alt="Foto 2" className="w-full h-full object-cover" onClick={() => {
+                    setIsOpen(true);
+                    setOpenOn(1);
+                }}/>
             </div>
 
             <div>
-                <img src={photos[2]} alt="Foto 3" className="w-full h-full object-cover"/>
+                <img src={photos[2]} alt="Foto 3" className="w-full h-full object-cover" onClick={() => {
+                    setIsOpen(true);
+                    setOpenOn(2);
+                }}/>
             </div>
 
             {/* Direita baixo */}
             <div>
-                <img src={photos[3]} alt="Foto 4" className="w-full h-full object-cover"/>
+                <img src={photos[3]} alt="Foto 4" className="w-full h-full object-cover" onClick={() => {
+                    setIsOpen(true);
+                    setOpenOn(3);
+                }}/>
             </div>
 
             <div className="relative">
-                <img src={photos[4]} alt="Foto 5" className="w-full h-full object-cover"/>
+                <img src={photos[4]} alt="Foto 5" className="w-full h-full object-cover" onClick={() => {
+                    setIsOpen(true);
+                    setOpenOn(4);
+                }}/>
 
                 {/* opcional */}
-                {photos.length > 5 && (
-                <div className="absolute inset-0 bg-black/50 flex items-center justify-center">
+                {photos.length > photoLimit && (
+                <div className="absolute inset-0 bg-black/50 flex items-center justify-center" onClick={() => {
+                    setIsOpen(true);
+                    setOpenOn(photoLimit);
+                }}>
                     <span className="text-white font-semibold">
-                    +{photos.length - 5} fotos
+                    +{photos.length - photoLimit} fotos
                     </span>
                 </div>
                 )}

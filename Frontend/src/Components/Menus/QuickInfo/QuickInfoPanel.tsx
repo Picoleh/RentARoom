46220d4459d4e46bae090ed0d9f7bb227707c267
Picoleh <ltpicoli@gmail.com>
2026-06-17 12:31:23 +0200

    changed background and padding

--- Frontend/src/Components/Menus/QuickInfo/QuickInfoPanel.tsx
+++ Frontend/src/Components/Menus/QuickInfo/QuickInfoPanel.tsx
@@ -5,7 +5,8 @@ import { FaToiletsPortable, FaWifi } from "react-icons/fa6";
 
 export default function QuickInfoPanel() {
     return (
-        <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 gap-x-8 gap-y-8 py-4 md:py-6 lg:py-8 justify-center mt-auto h-full">
+        <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 gap-x-8 gap-y-8 p-4 justify-center mt-auto h-full 
+         bg-(--bg-light-100) rounded-3xl">
             <QuickInfoItem Icon={FaRulerCombined} text="20m²" />
             <QuickInfoItem Icon={FaBed} text="4 Bedrooms"/>
             <QuickInfoItem Icon={FaShower} text="2 Bathrooms"/>
